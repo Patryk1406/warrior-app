21e36ea18ce184ead82bfe2b5f034b77b053b400
Patryk Kuropatwiński <patryk.kuropatwinski@gmail.com>
2022-02-09 03:17:34 +0100

    add api for the arena

--- routes/arenaRouter.ts
+++ routes/arenaRouter.ts
@@ -1,7 +1,25 @@
 import {Router} from "express";
+import {Arena} from "../repositories/arena";
+import {WarriorRecord} from "../records/warriorRecord";
 
 export const arenaRouter = Router();
 
-arenaRouter.get('/', (req, res) => {
 
+
+arenaRouter
+    .get('/', async (req, res) => {
+        const warriors = await WarriorRecord.getAll();
+        res.render('arena/main', {
+            warriors
+        });
+})
+    .get('/result', async (req, res) => {
+    const {firstWarriorId, secondWarriorId} = req.body;
+    const firstWarrior = await WarriorRecord.getOne(firstWarriorId);
+    const secondWarrior = await WarriorRecord.getOne(secondWarriorId);
+    const arena = new Arena(firstWarriorId, secondWarriorId);
+    const descriptionOfTheFight = await arena.fight();
+    res.render('arena/fightResult', {
+        descriptionOfTheFight,
+    })
 })
