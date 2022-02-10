45d526737799c2d3690ba7ecf8f1a70a0b026cf1
Patryk Kuropatwiński <patryk.kuropatwinski@gmail.com>
2022-02-10 18:43:33 +0100

    a slight improvement of the error handler

--- utils/error.ts
+++ utils/error.ts
@@ -3,6 +3,7 @@ import {NextFunction, Request, Response} from "express";
 export class InvalidDataError extends Error {}
 
 export function errorHandler(err: Error, req: Request, res: Response, next: NextFunction) {
+    console.error(err);
     let status;
     let message;
     if(err instanceof InvalidDataError) {
